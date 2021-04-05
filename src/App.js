d72af2de7939655bb35a45c54dd5c97a92ce849b
sil Medina <medina.silviapatricia@gmail.com>
2021-04-04 23:42:50 -0300

    agregue state al ejercicio 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Hello from './components/Hello';
 
 function App() {
+  const [msj, setMsj] =  useState('my friend!');
+  const handleClick = () =>{
+    setMsj('my friend (from changed state)!');
+  }
+  
   return (
     <Fragment>
-      <Hello nombre='my friend!'/>
+      <Hello nombre={msj}/>
+      <button type='button'onClick={handleClick}>Click me</button>
     </Fragment>
   );
 }
